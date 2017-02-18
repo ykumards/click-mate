05ec54d149c18ae06842bf7e634041d03f1a7718
preeti1205 <preeti.bhandari1205@gmail.com>
2017-02-18 14:54:16 -0500

    testing on Hello World app

--- identify.py
+++ identify.py
@@ -1,18 +1,18 @@
-# from flask import Flask
-from flask import Flask, jsonify, request
-from detect import predictor
+from flask import Flask
+# from flask import Flask, jsonify, request
+# from detect import predictor
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-@app.route('/identify', methods = ['GET'])
-def identify():
-    headline = request.args.get('headline', '')
-    clickbait_percent = predictor.predict(headline)
-    return jsonify({'clickbait percent': clickbait_percent * 100})
+# @app.route('/identify', methods = ['GET'])
+# def identify():
+#     headline = request.args.get('headline', '')
+#     clickbait_percent = predictor.predict(headline)
+#     return jsonify({'clickbait percent': clickbait_percent * 100})
 
-if __name__ == "__main__":
-	app.run()
+# if __name__ == "__main__":
+# 	app.run()
